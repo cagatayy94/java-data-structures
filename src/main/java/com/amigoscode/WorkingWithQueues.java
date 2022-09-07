0741d10d6c3ead459b327d1fdb91dd8384d77ef2
cagatay yilmaz <mustafa.cagatay.yilmaz@gmail.com>
2022-09-08 00:55:36 +0300

    Added linked list example

--- src/main/java/com/amigoscode/WorkingWithQueues.java
+++ src/main/java/com/amigoscode/WorkingWithQueues.java
@@ -1,10 +1,28 @@
 package com.amigoscode;
 
 import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.Queue;
 
 public class WorkingWithQueues {
     public static void main(String[] args) {
+        LinkedList<Person> linkedList = new LinkedList<>();
+        linkedList.add(new Person("Alex", 21));
+        linkedList.add(new Person("Alexa", 21));
+        linkedList.addFirst(new Person("Ali", 25));
+        linkedList.addLast(new Person("Aliye", 22));
+        ListIterator<Person> personListIterator = linkedList.listIterator();
+
+        while (personListIterator.hasNext()){
+            System.out.println(personListIterator.next());
+        }
+
+        while (personListIterator.hasPrevious()){
+            System.out.println(personListIterator.previous());
+        }
+    }
+
+    private static void queues() {
         Queue<Person> superMarket = new LinkedList<>();
 
         superMarket.add(new Person("Alex", 21));
@@ -16,7 +34,6 @@ public class WorkingWithQueues {
         System.out.println(superMarket.poll());
         System.out.println(superMarket.size());
         System.out.println(superMarket.peek());
-
     }
 
     static record Person(String name, int age){}
